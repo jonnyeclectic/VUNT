423d1ae861f456da93ef462aa7662ab83e5cf6da
ccf0056 <fitchchristian93@gmail.com>
2014-11-22 02:07:30 -0600

    Started Applications

---
+++ application/controllers/application.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Election extends CI_Controller {
+	
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('ion_auth');
+		$this->load->library('form_validation');
+		$this->load->database();
+		$this->load->library(array('ion_auth','form_validation'));
+		$this->load->helper(array('url','language'));
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+		$this->lang->load('auth');
+		$this->load->view('auth/home');
+	}
+	
+	function index()
+	{
+		
+	}
+}
