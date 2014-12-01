9fbfe83392607467e7c5c5bb875b60318a3f06b6
ccf0056 <fitchchristian93@gmail.com>
2014-12-01 03:13:44 -0600

    Updated Validation Tests

--- application/controllers/tests.php
+++ application/controllers/tests.php
@@ -1,6 +1,7 @@
 <?php 
 include 'application\controllers\auth.php';
-// Home page, really serving more of a placeholder function, but displays many options.
+include 'application\controllers\election.php';
+// Unit Tests are held here.
 class Tests extends CI_Controller {
 	
 	function __construct(){
@@ -17,6 +18,14 @@ class Tests extends CI_Controller {
 	
 	function index(){
 		$this->unit->run($this->ion_auth->elections(), 'is_array', 'Elections Exist');
+		
+		// Unit tests for new requirements
+		$this->unit->run($this->ion_auth->get_emails(0), 'is_array', 'Emails Return Correctly');
+		$this->unit->run($this->ion_auth->name_election(0), 'is_string', 'Elections Properly Named');
+		$confirmation_test_get = $this->db->get('votes');
+		foreach ($confirmation_test_get->result() as $row)
+			$this->unit->run($row->confirmation, 'is_string', 'Confirmation Numbers Exist');
+		
 		echo $this->unit->report();
 	}
 }
